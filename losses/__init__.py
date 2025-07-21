8ef0f5040513ef425322a02b32535198054b1893
Laura de Oliveira Lopes Henchman Balbi <2658323+lbalbi@users.noreply.github.com>
2025-07-21 19:04:29 +0000

    commit

--- losses/__init__.py
+++ losses/__init__.py
@@ -1,5 +1,6 @@
 from .triplet_margin import TripletLoss
 from .dualcontrastive_CE import DualContrastiveLoss_CE
-from .dualcontrastive import DualContrastiveLoss
+from .dualcontrastive_margin import DualContrastiveLoss
 from .multicontrastive import MultiContrastiveLoss
 from .contrastive_CE import ContrastiveLoss
+from .contrastive_margin import ContrastiveLossMargin
--- losses/contrastive_margin.py
+++ losses/contrastive_margin.py
@@ -3,15 +3,15 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class ContrastiveLoss(nn.Module):
+class ContrastiveLossMargin(nn.Module):
     def __init__(self, margin: float = 1.0):
         super().__init__()
-        self.margin = margin # margin-based losses use hard cut-off value to separate positive and negative pairs
+        self.margin = margin 
+        # margin-based losses use hard cut-off value to separate positive and negative pairs
 
     def forward(self, z1, z2, labels):
 
         distances = F.pairwise_distance(z1, z2, p=2)
-
         positive_loss = labels * distances.pow(2)
         negative_loss = (1 - labels) * F.relu(self.margin - distances).pow(2)
         loss = 0.5 * (positive_loss + negative_loss).mean()
