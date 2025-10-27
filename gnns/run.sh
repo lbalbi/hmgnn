f6e8df07f0539ad13f1bdb7140a40247a0e1cf53
Laura Balbi <72658323+lbalbi@users.noreply.github.com>
2025-10-27 19:07:48 +0000

    Update run.sh

--- gnns/run.sh
+++ gnns/run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=GCN
+#SBATCH --job-name=gnn_pyg
 #SBATCH --nodelist=opel
-#SBATCH --output=./GCN_melanogaster.txt
+#SBATCH --output=./gnn_pyg.txt
 #SBATCH --ntasks=1
 #SBATCH --time=30:00:00
 #SBATCH --partition=compute
 
 echo "Job running on node: $(hostname)"
 
-python gnn_dgl.py -model GCN
+python gnn_pyg.py -model GCN
