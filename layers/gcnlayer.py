958d1ab69cb0da58229011bc2c9cb6d74a9f402a
Laura Balbi <72658323+lbalbi@users.noreply.github.com>
2025-10-27 19:41:07 +0000

    Update gcnlayer.py

--- layers/gcnlayer.py
+++ layers/gcnlayer.py
@@ -1,12 +1,20 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch_geometric.nn import MessagePassing
+from torch_geometric.utils import softmax
 
-class GCNLayer(nn.Module):
+
+class GCNLayer(MessagePassing):
+    """
+    Bipartite-capable attention layer (PyG).
+    If x is a Tensor -> homogeneous case.
+    If x is a tuple (x_src, x_dst) -> bipartite case.
+    """
     def __init__(self, in_dim, out_dim):
-        super(GCNLayer, self).__init__()
-        self.fc1 = nn.Linear(in_dim, out_dim, bias=False)
-        self.fc2 = nn.Linear(in_dim, out_dim, bias=False)
+        super().__init__(aggr='add')  # sum aggregation
+        self.fc1 = nn.Linear(in_dim, out_dim, bias=False)   # for src
+        self.fc2 = nn.Linear(in_dim, out_dim, bias=False)   # for dst (used in bipartite)
         self.attn_fc = nn.Linear(2 * out_dim, 1, bias=False)
         self.reset_parameters()
 
@@ -16,88 +24,80 @@ class GCNLayer(nn.Module):
         nn.init.xavier_normal_(self.fc2.weight, gain=gain)
         nn.init.xavier_normal_(self.attn_fc.weight, gain=gain)
 
-    def edge_attention(self, edges):
-        z2 = torch.cat([edges.src["z"], edges.dst["z"]], dim=1)
-        e  = self.attn_fc(z2)
-        return {"e": F.leaky_relu(e)}
-
-    def message_func(self, edges):
-        return {"z": edges.src["z"], "e": edges.data["e"]}
-
-    def reduce_func(self, nodes):
-        alpha = F.softmax(nodes.mailbox["e"], dim=1)
-        h     = torch.sum(alpha * nodes.mailbox["z"], dim=1)
-        return {"h": h}
-
-    def forward(self, g, h):
-
-        if isinstance(h, tuple):
-            h_src, h_dst = h
-            z_src = self.fc1(h_src)
-            z_dst = self.fc2(h_dst)
-            g.srcdata["z"] = z_src
-            g.dstdata["z"] = z_dst
+    def forward(self, x, edge_index, size=None):
+        """
+        x: Tensor [N,F] or (x_src [Ns,F], x_dst [Nd,F])
+        edge_index: LongTensor [2,E]
+        size: optional (Ns, Nd) for bipartite
+        """
+        if isinstance(x, tuple):
+            x_src, x_dst = x
+            z_src = self.fc1(x_src)
+            z_dst = self.fc2(x_dst)
+            out = self.propagate(edge_index, x=(z_src, z_dst), size=size)  # -> [Nd, out_dim]
         else:
-            z = self.fc1(h)
-            g.ndata["z"] = z
+            z = self.fc1(x)  # [N, out_dim]
+            out = self.propagate(edge_index, x=z)  # homogeneous -> [N, out_dim]
+        return F.relu(out)
 
-        g.apply_edges(self.edge_attention)
-        g.update_all(self.message_func, self.reduce_func)
-        if isinstance(h, tuple):
-            return g.dstdata.pop("h")
-        else:
-            return g.ndata.pop("h")
+    def message(self, x_j, x_i, index):
+        """
+        x_j: source node transformed features [E, out_dim]
+        x_i: target node transformed features [E, out_dim]
+        index: target indices per edge (for softmax)
+        """
+        e = self.attn_fc(torch.cat([x_j, x_i], dim=-1))  # [E,1]
+        a = F.leaky_relu(e)
+        alpha = softmax(a, index)                        # normalize over incoming edges of each dst
+        return alpha * x_j                               # weighted messages
+
+    def update(self, aggr_out):
+        return aggr_out
 
 
-
-class GCNLayer_homogeneous(nn.Module):
+class GCNLayer_homogeneous(MessagePassing):
+    """
+    Homogeneous attention layer (PyG).
+    """
     def __init__(self, in_dim, out_dim):
-        super(GCNLayer_homogeneous, self).__init__()
+        super().__init__(aggr='add')
         self.fc = nn.Linear(in_dim, out_dim, bias=False)
         self.attn_fc = nn.Linear(2 * out_dim, 1, bias=False)
         self.reset_parameters()
 
     def reset_parameters(self):
-        """Reinitialize learnable parameters."""
         gain = nn.init.calculate_gain("relu")
         nn.init.xavier_normal_(self.fc.weight, gain=gain)
         nn.init.xavier_normal_(self.attn_fc.weight, gain=gain)
 
-    def edge_attention(self, edges):
-        z2 = torch.cat([edges.src["z"], edges.dst["z"]], dim=1)
-        a = self.attn_fc(z2)
-        return {"e": F.leaky_relu(a)}
+    def forward(self, x, edge_index):
+        z = self.fc(x)  # [N, out_dim]
+        out = self.propagate(edge_index, x=z)  # [N, out_dim]
+        return F.relu(out)
 
-    def message_func(self, edges):
-        return {"z": edges.src["z"], "e": edges.data["e"]}
+    def message(self, x_j, x_i, index):
+        e = self.attn_fc(torch.cat([x_j, x_i], dim=-1))  # [E,1]
+        a = F.leaky_relu(e)
+        alpha = softmax(a, index)
+        return alpha * x_j
 
-    def reduce_func(self, nodes):
-        alpha = F.softmax(nodes.mailbox["e"], dim=1)
-        h = torch.sum(alpha * nodes.mailbox["z"], dim=1)
-        return {"h": h}
-
-    def forward(self, g, h):
-
-        z = self.fc(h)
-        g.ndata["z"] = z
-        g.apply_edges(self.edge_attention)
-        g.update_all(self.message_func, self.reduce_func)
-        return g.ndata.pop("h")
-
+    def update(self, aggr_out):
+        return aggr_out
 
 
 class GCNLayer_(nn.Module):
+    """
+    Pure MLP over node features; already PyG-compatible.
+    """
     def __init__(self, in_dim, out_dim):
         super(GCNLayer_, self).__init__()
         self.linear = nn.Linear(in_dim, out_dim)
         self.reset_parameters()
     
     def reset_parameters(self):
-        """Reinitialize learnable parameters."""
         self.linear.reset_parameters()
         gain = nn.init.calculate_gain("relu")
         nn.init.xavier_normal_(self.linear.weight, gain=gain)
 
     def forward(self, h):
-        h = self.linear(h)
-        return F.relu(h)
+        return F.relu(self.linear(h))
