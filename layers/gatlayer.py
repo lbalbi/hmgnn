e45a1b5439dd72e84d2e4792df1d828496a44bb1
Laura Balbi <72658323+lbalbi@users.noreply.github.com>
2025-10-27 19:46:22 +0000

    Update gatlayer.py

--- layers/gatlayer.py
+++ layers/gatlayer.py
@@ -1,51 +1,58 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch_geometric.nn import MessagePassing
+from torch_geometric.utils import softmax
 
-class GATLayer(nn.Module):
+
+class GATLayer(MessagePassing):
+    """
+    Homogeneous single-head GAT layer. Use with
+    'out = layer(x, edge_index)', where x: [N, Fin], edge_index: [2, E]
+    """
     def __init__(self, in_dim, out_dim):
-        super(GATLayer, self).__init__()
+        super().__init__(aggr='mean')
         self.fc = nn.Linear(in_dim, out_dim, bias=False)
         self.attn_fc = nn.Linear(2 * out_dim, 1, bias=False)
         self.reset_parameters()
 
     def reset_parameters(self):
-        """Reinitialize learnable parameters."""
         gain = nn.init.calculate_gain("relu")
         nn.init.xavier_normal_(self.fc.weight, gain=gain)
         nn.init.xavier_normal_(self.attn_fc.weight, gain=gain)
 
-    def edge_attention(self, edges):
-        z2 = torch.cat([edges.src["z"], edges.dst["z"]], dim=1)
-        a = self.attn_fc(z2)
-        return {"e": F.leaky_relu(a)}
+    def forward(self, x, edge_index):
+        """
+        x: [N, Fin], edge_index: [2, E]
+        """
+        z = self.fc(x)
+        out = self.propagate(edge_index, x=z)
+        return F.relu(out)
 
-    def message_func(self, edges):
-        return {"z": edges.src["z"], "e": edges.data["e"]}
+    def message(self, x_j, x_i, index):
+        """
+        x_j: source node features (after fc) [E, Fout]
+        x_i: target node features (after fc) [E, Fout]
+        index: target indices per edge, used for attention softmax
+        """
+        e = self.attn_fc(torch.cat([x_j, x_i], dim=-1))
+        a = F.leaky_relu(e)
+        alpha = softmax(a, index)
+        return alpha * x_j
 
-    def reduce_func(self, nodes):
-        alpha = F.softmax(nodes.mailbox["e"], dim=1)
-        h = torch.sum(alpha * nodes.mailbox["z"], dim=1)
-        return {"h": h}
+    def update(self, aggr_out):
+        return aggr_out
 
-    def forward(self, g, h):
-        z = self.fc(h)
-        g.ndata["z"] = z
-        g.apply_edges(self.edge_attention)
-        g.update_all(self.message_func, self.reduce_func)
-        return g.ndata.pop("h")
 
 class GATLayer_(nn.Module):
     def __init__(self, in_dim, out_dim):
-        super(GATLayer, self).__init__()
+        super(GATLayer_, self).__init__()
         self.linear = nn.Linear(in_dim, out_dim)
         self.reset_parameters()
     
     def reset_parameters(self):
-        """Reinitialize learnable parameters."""
         gain = nn.init.calculate_gain("relu")
         nn.init.xavier_normal_(self.linear.weight, gain=gain)
 
     def forward(self, h):
-        h = self.linear(h)
-        return F.relu(h)
+        return F.relu(self.linear(h))
