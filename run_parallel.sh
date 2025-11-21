6c8c4c85394d2e550e9b841d457aa3929494f9be
lbalbi <72658323+lbalbi@users.noreply.github.com>
2025-11-21 11:56:57 +0000

    commit sh

--- run_parallel.sh
+++ run_parallel.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
-#SBATCH --job-name=cerevisae_noCont_withLC
-#SBATCH --array=1-10
+#SBATCH --job-name=cerevisae_withLC
+#SBATCH --array=1-5
 #SBATCH --output=slurm_log.txt
 #SBATCH --ntasks=1
 #SBATCH --time=30:00:00
-#SBATCH --nodelist=liseda-03
+#SBATCH --nodelist=liseda-05
 #SBATCH --partition=tier3
 
 set -euo pipefail
 
 RUN_TAG="run_${SLURM_ARRAY_TASK_ID}"
-LOG_DIR="output/cerevisae_noCont_withLC"
-OUTDIR="${LOG_DIR}/output_cerevisae_noCont_withLC_${RUN_TAG}"
-LOGFILE="${LOG_DIR}/output_cerevisae_noCont_withLC_${RUN_TAG}.txt"
+LOG_DIR="output/cerevisae_withLC"
+OUTDIR="${LOG_DIR}/output_cerevisae_withLC_${RUN_TAG}"
+LOGFILE="${LOG_DIR}/output_cerevisae_withLC_${RUN_TAG}.txt"
 mkdir -p "${OUTDIR}"
 
 python -u main.py \
-  --path "cerevisae_data" \
-  --no_contrastive \
+  --path "cerevisae_data_withLC" \
   --task "cerevisae" \
-  --output_dir "cerevisae_noCont_withLC/output_cerevisae_noCont_withLC_${RUN_TAG}/" \
+  --output_dir "cerevisae_withLC/output_cerevisae_withLC_${RUN_TAG}/" \
   >> "${LOGFILE}" 2>&1
