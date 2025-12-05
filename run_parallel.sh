8daf22fef96accff242f9aca1be0a12f5dacaf8a
lbalbi <72658323+lbalbi@users.noreply.github.com>
2025-12-05 10:17:33 +0000

    fixes

--- run_parallel.sh
+++ run_parallel.sh
@@ -7,17 +7,26 @@
 #SBATCH --nodelist=liseda-05
 #SBATCH --partition=tier3
 
+#SBATCH --job-name=human_test_shgcn_only_CLnegs
+#SBATCH --array=1-5
+#SBATCH --output=slurm_log.txt
+#SBATCH --ntasks=1
+#SBATCH --time=30:00:00
+#SBATCH --nodelist=liseda-03
+#SBATCH --partition=tier3
+
 set -euo pipefail
 
 RUN_TAG="run_${SLURM_ARRAY_TASK_ID}"
-LOG_DIR="output/human_test_shgcn_multi"
-OUTDIR="${LOG_DIR}/output_human_test_shgcn_multi${RUN_TAG}"
-LOGFILE="${LOG_DIR}/output_human_test_shgcn_multi${RUN_TAG}.txt"
+LOG_DIR="output/human_test_shgcn_only_CLnegs"
+OUTDIR="${LOG_DIR}/output_human_test_shgcn_only_CLnegs_${RUN_TAG}"
+LOGFILE="${LOG_DIR}/output_human_test_shgcn_only_CLnegs_${RUN_TAG}.txt"
 mkdir -p "${OUTDIR}"
 
 python -u main.py \
   --path "data/human_data_withLC" \
   --task "human" \
-  --model "shgcn" \
-  --output_dir "human_test_shgcn_multi/output_human_test_shgcn_multi${RUN_TAG}/" \
+  --use_nstatement_sampler \
+  --model shgcn \
+  --output_dir "human_test_shgcn_only_CLnegs/output_human_test_shgcn_only_CLnegs_${RUN_TAG}/" \
   >> "${LOGFILE}" 2>&1
