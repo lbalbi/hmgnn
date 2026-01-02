e24f1ab73df25f030822f2d8888b495be18ab291
lbalbi <72658323+lbalbi@users.noreply.github.com>
2026-01-02 22:20:18 +0000

    commit

--- run_parallel_2.sh
+++ run_parallel_2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=human_perProt_randomNegs_hgcn
+#SBATCH --job-name=human_perProtDeg_randomNegs_hgcn
 #SBATCH --array=1-10
 #SBATCH --output=slurm_log.txt
 #SBATCH --ntasks=1
@@ -10,15 +10,15 @@
 set -euo pipefail
 
 RUN_TAG="run_${SLURM_ARRAY_TASK_ID}"
-LOG_DIR="output/human_perProt_randomNegs_hgcn"
-OUTDIR="${LOG_DIR}/output_human_perProt_randomNegs_hgcn_${RUN_TAG}"
-LOGFILE="${LOG_DIR}/output_human_perProt_randomNegs_hgcn_${RUN_TAG}.txt"
+LOG_DIR="output/human_perProtDeg_randomNegs_hgcn"
+OUTDIR="${LOG_DIR}/output_human_perProtDeg_randomNegs_hgcn_${RUN_TAG}"
+LOGFILE="${LOG_DIR}/output_human_perProtDeg_randomNegs_hgcn_${RUN_TAG}.txt"
 mkdir -p "${OUTDIR}"
 
 python -u main.py \
   --path "data/human_data_withLC" \
   --task "human" \
-  --protein_splits \
+  --protein_degree_splits \
   --use_rstatement_sampler \
-  --output_dir "human_perProt_randomNegs_hgcn/output_human_perProt_randomNegs_hgcn_${RUN_TAG}/" \
+  --output_dir "human_perProtDeg_randomNegs_hgcn/output_human_perProtDeg_randomNegs_hgcn_${RUN_TAG}/" \
   >> "${LOGFILE}" 2>&1
