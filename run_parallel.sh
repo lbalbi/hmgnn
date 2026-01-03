bf4d2f86e88d0774eb202ae328d9079049bc8ffe
lbalbi <72658323+lbalbi@users.noreply.github.com>
2026-01-03 10:34:51 +0000

    commit

--- run_parallel.sh
+++ run_parallel.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
-#SBATCH --job-name=human_test_shgcn_protCL_perPPI
+#SBATCH --job-name=human_shgcn_randomNegs_perProt
 #SBATCH --array=1-5
 #SBATCH --output=slurm_log.txt
 #SBATCH --ntasks=1
 #SBATCH --time=30:00:00
-#SBATCH --nodelist=liseda-05
+#SBATCH --nodelist=liseda-01
 #SBATCH --partition=tier3
 
 set -euo pipefail
 
 RUN_TAG="run_${SLURM_ARRAY_TASK_ID}"
-LOG_DIR="output/human_test_shgcn_protCL_perPPI"
-OUTDIR="${LOG_DIR}/output_human_test_shgcn_protCL_perPPI_${RUN_TAG}"
-LOGFILE="${LOG_DIR}/output_human_test_shgcn_protCL_perPPI_${RUN_TAG}.txt"
+LOG_DIR="output/human_shgcn_randomNegs_perProt"
+OUTDIR="${LOG_DIR}/output_human_shgcn_randomNegs_perProt_${RUN_TAG}"
+LOGFILE="${LOG_DIR}/output_human_shgcn_randomNegs_perProt_${RUN_TAG}.txt"
 mkdir -p "${OUTDIR}"
 
 python -u main.py \
   --path "data/human_data_withLC" \
   --task "human" \
   --use_protein_contrastive \
+  --use_rstatement_sampler \
+  --protein_splits \
   --model shgcn \
-  --output_dir "human_test_shgcn_protCL_perPPI/output_human_test_shgcn_protCL_perPPI_${RUN_TAG}/" \
+  --output_dir "human_shgcn_randomNegs_perProt/output_human_shgcn_randomNegs_perProt_${RUN_TAG}/" \
   >> "${LOGFILE}" 2>&1
