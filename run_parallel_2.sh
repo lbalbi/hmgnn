47474e5bfb985a41a72971a6fcf5d5e1b4eae8c3
lbalbi <72658323+lbalbi@users.noreply.github.com>
2026-01-03 10:31:51 +0000

    commit

--- run_parallel_2.sh
+++ run_parallel_2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=human_perProtDeg_randomNegs_hgcn
-#SBATCH --array=1-10
+#SBATCH --array=2-10
 #SBATCH --output=slurm_log.txt
 #SBATCH --ntasks=1
 #SBATCH --time=30:00:00
--- run_parallel_3.sh
+++ run_parallel_3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=human_perPPI_randomNegs_hgcn
-#SBATCH --array=1-10
+#SBATCH --array=5-10
 #SBATCH --output=slurm_log.txt
 #SBATCH --ntasks=1
 #SBATCH --time=30:00:00
