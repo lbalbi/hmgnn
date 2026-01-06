d1346bf415c4cc8333f171dfa59af68f48daee88
lbalbi <72658323+lbalbi@users.noreply.github.com>
2026-01-06 14:55:48 +0000

    commit

--- run_parallel_3.sh
+++ run_parallel_3.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
-#SBATCH --job-name=human_perPPI_randomNegs_hgcn
-#SBATCH --array=5-10
+#SBATCH --job-name=human_perProt_gcn
+#SBATCH --array=2-2
 #SBATCH --output=slurm_log.txt
 #SBATCH --ntasks=1
 #SBATCH --time=30:00:00
-#SBATCH --nodelist=liseda-05
+#SBATCH --nodelist=liseda-t1
 #SBATCH --partition=tier3
 
 set -euo pipefail
 
 RUN_TAG="run_${SLURM_ARRAY_TASK_ID}"
-LOG_DIR="output/human_perPPI_randomNegs_hgcn"
-OUTDIR="${LOG_DIR}/output_human_perPPI_randomNegs_hgcn_${RUN_TAG}"
-LOGFILE="${LOG_DIR}/output_human_perPPI_randomNegs_hgcn_${RUN_TAG}.txt"
+LOG_DIR="output/human_perProt_gcn"
+OUTDIR="${LOG_DIR}/output_human_perProt_gcn_${RUN_TAG}"
+LOGFILE="${LOG_DIR}/output_human_perProt_gcn_${RUN_TAG}.txt"
 mkdir -p "${OUTDIR}"
 
 python -u main.py \
   --path "data/human_data_withLC" \
   --task "human" \
-  --use_rstatement_sampler \
-  --patience 30 \
-  --output_dir "human_perPPI_randomNegs_hgcn/output_human_perPPI_randomNegs_hgcn_${RUN_TAG}/" \
+  --no_contrastive \
+  --model "gcn" \
+  --protein_splits \
+  --patience 25 \
+  --output_dir "human_perProt_gcn/output_human_perProt_gcn_${RUN_TAG}/" \
   >> "${LOGFILE}" 2>&1
