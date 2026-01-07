555424e6295ac0504b7f19150ee4a34abce5d3b9
lbalbi <72658323+lbalbi@users.noreply.github.com>
2026-01-07 21:57:15 +0000

    commit

--- run_parallel_2.sh
+++ run_parallel_2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=wikidata_rahgcn_noNegsCL_NEW
-#SBATCH --array=1-1
+#SBATCH --array=2-2
 #SBATCH --output=slurm_log.txt
 #SBATCH --ntasks=1
 #SBATCH --time=45:00:00
@@ -20,5 +20,8 @@ python -u main.py \
   --model "ra_hgcn" \
   --use_nstatementsampler \
   --batch_size 3024 \
+  --finaltrain_only \
+  --final_lr 0.005 \
+  --final_epochs 38 \
   --output_dir "wikidata_rahgcn_noNegsCL_NEW/output_wikidata_rahgcn_noNegsCL_NEW_${RUN_TAG}/" \
   >> "${LOGFILE}" 2>&1
