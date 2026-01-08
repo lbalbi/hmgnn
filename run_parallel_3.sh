2ff281fc6b9974eb8eccb069a699df2e98a140d2
lbalbi <72658323+lbalbi@users.noreply.github.com>
2026-01-08 08:58:50 +0000

    commit

--- run_parallel_3.sh
+++ run_parallel_3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=huri_perPPI_hgcn
-#SBATCH --array=3-3
+#SBATCH --job-name=huri_perPPI_hgcn_ProtCL
+#SBATCH --array=1-1
 #SBATCH --output=slurm_log.txt
 #SBATCH --ntasks=1
 #SBATCH --time=30:00:00
@@ -10,16 +10,14 @@
 set -euo pipefail
 
 RUN_TAG="run_${SLURM_ARRAY_TASK_ID}"
-LOG_DIR="output/huri_perPPI_hgcn"
-OUTDIR="${LOG_DIR}/output_huri_perPPI_hgcn_${RUN_TAG}"
-LOGFILE="${LOG_DIR}/output_huri_perPPI_hgcn_${RUN_TAG}.txt"
+LOG_DIR="output/huri_perPPI_hgcn_ProtCL"
+OUTDIR="${LOG_DIR}/output_huri_perPPI_hgcn_ProtCL_${RUN_TAG}"
+LOGFILE="${LOG_DIR}/output_huri_perPPI_hgcn_ProtCL_${RUN_TAG}.txt"
 mkdir -p "${OUTDIR}"
 
 python -u main.py \
   --path "data/huri_data_withLC" \
   --task "huri" \
-  --no_contrastive \
   --model "hgcn" \
-  --patience 25 \
-  --output_dir "huri_perPPI_hgcn/output_huri_perPPI_hgcn_${RUN_TAG}/" \
+  --output_dir "huri_perPPI_hgcn_ProtCL/output_huri_perPPI_hgcn_ProtCL_${RUN_TAG}/" \
   >> "${LOGFILE}" 2>&1
