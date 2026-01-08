8bab776cc122641b9c7e86549b5e519a508e1f6d
lbalbi <72658323+lbalbi@users.noreply.github.com>
2026-01-08 08:56:33 +0000

    commit

--- run_parallel_2.sh
+++ run_parallel_2.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
-#SBATCH --job-name=wikidata_rahgcn_noNegsCL_NEW
+#SBATCH --job-name=wikidata_srahgcn_ProtCL_NEW_2
 #SBATCH --array=2-2
 #SBATCH --output=slurm_log.txt
 #SBATCH --ntasks=1
 #SBATCH --time=45:00:00
-#SBATCH --nodelist=liseda-03
+#SBATCH --nodelist=liseda-01
 #SBATCH --partition=tier3
 
 set -euo pipefail
+
 RUN_TAG="run_${SLURM_ARRAY_TASK_ID}"
-LOG_DIR="output/wikidata_rahgcn_noNegsCL_NEW"
-OUTDIR="${LOG_DIR}/output_wikidata_rahgcn_noNegsCL_NEW_${RUN_TAG}"
-LOGFILE="${LOG_DIR}/output_wikidata_rahgcn_noNegsCL_NEW_${RUN_TAG}.txt"
+LOG_DIR="output/wikidata_srahgcn_ProtCL_NEW"
+OUTDIR="${LOG_DIR}/output_wikidata_srahgcn_ProtCL_NEW_${RUN_TAG}"
+LOGFILE="${LOG_DIR}/output_wikidata_srahgcn_ProtCL_NEW_${RUN_TAG}.txt"
 mkdir -p "${OUTDIR}"
 
 python -u main.py \
   --path "data/wikidata_data" \
   --task "wikidata" \
-  --model "ra_hgcn" \
-  --use_nstatementsampler \
+  --model "sra_hgcn" \
   --batch_size 3024 \
-  --finaltrain_only \
-  --final_lr 0.005 \
-  --final_epochs 38 \
-  --output_dir "wikidata_rahgcn_noNegsCL_NEW/output_wikidata_rahgcn_noNegsCL_NEW_${RUN_TAG}/" \
+  --epochs 200 \
+  --output_dir "wikidata_srahgcn_ProtCL_NEW/output_wikidata_srahgcn_ProtCL_NEW_${RUN_TAG}/" \
   >> "${LOGFILE}" 2>&1
