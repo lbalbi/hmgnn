a34a4177ca9e7042572c2fc09b901fe328c24d8c
lbalbi <72658323+lbalbi@users.noreply.github.com>
2026-01-09 14:25:32 +0000

    commit

--- run_parallel_2.sh
+++ run_parallel_2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=wikidata_srahgcn_RandomNegs_NEW
-#SBATCH --array=1-2
+#SBATCH --job-name=wikidata_noCL_NEW
+#SBATCH --array=3-3
 #SBATCH --output=slurm_log.txt
 #SBATCH --ntasks=1
 #SBATCH --time=45:00:00
@@ -10,16 +10,17 @@
 set -euo pipefail
 
 RUN_TAG="run_${SLURM_ARRAY_TASK_ID}"
-LOG_DIR="output/wikidata_srahgcn_RandomNegs_NEW"
-OUTDIR="${LOG_DIR}/output_wikidata_srahgcn_RandomNegs_NEW_${RUN_TAG}"
-LOGFILE="${LOG_DIR}/output_wikidata_srahgcn_RandomNegs_NEW_${RUN_TAG}.txt"
+LOG_DIR="output/wikidata_noCL_NEW"
+OUTDIR="${LOG_DIR}/output_wikidata_noCL_NEW_${RUN_TAG}"
+LOGFILE="${LOG_DIR}/output_wikidata_noCL_NEW_${RUN_TAG}.txt"
 mkdir -p "${OUTDIR}"
 
 python -u main.py \
   --path "data/wikidata_data" \
   --task "wikidata" \
-  --model "sra_hgcn" \
+  --model "ra_hgcn" \
   --batch_size 3024 \
-  --use_rstatement_sampler \
-  --output_dir "wikidata_srahgcn_RandomNegs_NEW/output_wikidata_srahgcn_RandomNegs_NEW_${RUN_TAG}/" \
+  --epochs 200 \
+  --no_contrastive \
+  --output_dir "wikidata_noCL_NEW/output_wikidata_noCL_NEW_${RUN_TAG}/" \
   >> "${LOGFILE}" 2>&1
