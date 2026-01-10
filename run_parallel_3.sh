64bf95af8001f66871cbd74ec17299cef1b7971f
lbalbi <72658323+lbalbi@users.noreply.github.com>
2026-01-10 01:17:08 +0000

    commit

--- run_parallel_3.sh
+++ run_parallel_3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=human_sgnn_perProt_NEW
-#SBATCH --array=2-2
+#SBATCH --job-name=human_sgat_perProt_NEW
+#SBATCH --array=1-1
 #SBATCH --output=slurm_log.txt
 #SBATCH --ntasks=1
 #SBATCH --time=30:00:00
@@ -10,16 +10,17 @@
 set -euo pipefail
 
 RUN_TAG="run_${SLURM_ARRAY_TASK_ID}"
-LOG_DIR="output/human_sgnn_perProt_NEW"
-OUTDIR="${LOG_DIR}/output_human_sgnn_perProt_NEW_${RUN_TAG}"
-LOGFILE="${LOG_DIR}/output_human_sgnn_perProt_NEW_${RUN_TAG}.txt"
+LOG_DIR="output/human_sgat_perProt_NEW"
+OUTDIR="${LOG_DIR}/output_human_sgat_perProt_NEW_${RUN_TAG}"
+LOGFILE="${LOG_DIR}/output_human_sgat_perProt_NEW_${RUN_TAG}.txt"
 mkdir -p "${OUTDIR}"
 
 python -u main.py \
   --path "data/human_data_withLC" \
   --task "human" \
-  --model "sgnn" \
+  --model "sgat" \
+  --batch_size 3024 \
   --protein_splits \
   --no_contrastive \
-  --output_dir "human_sgnn_perProt_NEW/output_human_sgnn_perProt_NEW_${RUN_TAG}/" \
+  --output_dir "human_sgat_perProt_NEW/output_human_sgat_perProt_NEW_${RUN_TAG}/" \
   >> "${LOGFILE}" 2>&1
