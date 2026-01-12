3280abc0310f4b223713b9fab2230f1dc2acec0f
lbalbi <72658323+lbalbi@users.noreply.github.com>
2026-01-12 13:22:34 +0000

    commit

--- run_parallel_2.sh
+++ run_parallel_2.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
-#SBATCH --job-name=wikidata_gae_NEW
-#SBATCH --array=1-3
+#SBATCH --job-name=wikidata_gcn_NEW
+#SBATCH --array=3-3
 #SBATCH --output=slurm_log.txt
 #SBATCH --ntasks=1
 #SBATCH --time=45:00:00
-#SBATCH --nodelist=liseda-01
+#SBATCH --nodelist=liseda-03
 #SBATCH --partition=tier3
 
 set -euo pipefail
 
 RUN_TAG="run_${SLURM_ARRAY_TASK_ID}"
-LOG_DIR="output/wikidata_gae_NEW"
-OUTDIR="${LOG_DIR}/output_wikidata_gae_NEW_${RUN_TAG}"
-LOGFILE="${LOG_DIR}/output_wikidata_gae_NEW_${RUN_TAG}.txt"
+LOG_DIR="output/wikidata_gcn_NEW"
+OUTDIR="${LOG_DIR}/output_wikidata_gcn_NEW_${RUN_TAG}"
+LOGFILE="${LOG_DIR}/output_wikidata_gcn_NEW_${RUN_TAG}.txt"
 mkdir -p "${OUTDIR}"
 
 python -u main.py \
   --path "data/wikidata_data" \
   --task "wikidata" \
-  --model "gae" \
+  --model "gcn" \
   --batch_size 3024 \
   --no_contrastive \
   --test_only \
-  --output_dir "wikidata_gae_NEW/output_wikidata_gae_NEW_${RUN_TAG}/" \
+  --output_dir "wikidata_gcn_NEW/output_wikidata_gcn_NEW_${RUN_TAG}/" \
   >> "${LOGFILE}" 2>&1
--- run_parallel_3.sh
+++ run_parallel_3.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-#SBATCH --job-name=wikidata_rahgcn_noNegs_RandomNegs_NEW
-#SBATCH --array=1-3
+#SBATCH --job-name=wikidata_noCL_NEW
+#SBATCH --array=5-5
 #SBATCH --output=slurm_log.txt
 #SBATCH --ntasks=1
 #SBATCH --time=45:00:00
-#SBATCH --nodelist=liseda-03
+#SBATCH --nodelist=liseda-01
 #SBATCH --partition=tier3
 
 set -euo pipefail
 
 RUN_TAG="run_${SLURM_ARRAY_TASK_ID}"
-LOG_DIR="output/wikidata_rahgcn_noNegs_RandomNegs_NEW"
-OUTDIR="${LOG_DIR}/output_wikidata_rahgcn_noNegs_RandomNegs_NEW_${RUN_TAG}"
-LOGFILE="${LOG_DIR}/output_wikidata_rahgcn_noNegs_RandomNegs_NEW_${RUN_TAG}.txt"
+LOG_DIR="output/wikidata_noCL_NEW"
+OUTDIR="${LOG_DIR}/output_wikidata_noCL_NEW_${RUN_TAG}"
+LOGFILE="${LOG_DIR}/output_wikidata_noCL_NEW_${RUN_TAG}.txt"
 mkdir -p "${OUTDIR}"
 
 python -u main.py \
@@ -20,7 +20,7 @@ python -u main.py \
   --task "wikidata" \
   --model "ra_hgcn" \
   --batch_size 3024 \
-  --use_rstatement_sampler \
-  --use_nstatementsampler \
-  --output_dir "wikidata_rahgcn_noNegs_RandomNegs_NEW/output_wikidata_rahgcn_noNegs_RandomNegs_NEW_${RUN_TAG}/" \
+  --no_contrastive \
+  --test_only \
+  --output_dir "wikidata_noCL_NEW/output_wikidata_noCL_NEW_${RUN_TAG}/" \
   >> "${LOGFILE}" 2>&1
