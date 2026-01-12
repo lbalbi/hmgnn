c616537887af3955a864ba2d02a06801cd3edbeb
lbalbi <72658323+lbalbi@users.noreply.github.com>
2026-01-12 11:53:03 +0000

    commit

--- run_parallel.sh
+++ run_parallel.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
-#SBATCH --job-name=wikidata_sgat_NEW
-#SBATCH --array=5-5
+#SBATCH --job-name=wikidata_rahgcn_protCL
+#SBATCH --array=4-4
 #SBATCH --output=slurm_log.txt
 #SBATCH --ntasks=1
-#SBATCH --time=45:00:00
-#SBATCH --nodelist=liseda-01
+#SBATCH --time=30:00:00
+#SBATCH --nodelist=liseda-03
 #SBATCH --partition=tier3
 
 set -euo pipefail
 
 RUN_TAG="run_${SLURM_ARRAY_TASK_ID}"
-LOG_DIR="output/wikidata_sgat_NEW"
-OUTDIR="${LOG_DIR}/output_wikidata_sgat_NEW_${RUN_TAG}"
-LOGFILE="${LOG_DIR}/output_wikidata_sgat_NEW_${RUN_TAG}.txt"
+LOG_DIR="output/wikidata_protCL_NEW"
+OUTDIR="${LOG_DIR}/output_wikidata_protCL_NEW_${RUN_TAG}"
+LOGFILE="${LOG_DIR}/output_wikidata_protCL_NEW_${RUN_TAG}.txt"
 mkdir -p "${OUTDIR}"
 
 python -u main.py \
   --path "data/wikidata_data" \
   --task "wikidata" \
-  --model "sgat" \
+  --model "ra_hgcn" \
   --batch_size 3024 \
-  --no_contrastive \
   --finaltrain_only \
-  --final_lr 0.005 \
-  --final_epochs 35 \
-  --output_dir "wikidata_sgat_NEW/output_wikidata_sgat_NEW_${RUN_TAG}/" \
+  --final_lr 0.05 \
+  --final_epochs 50 \
+  --output_dir "wikidata_protCL_NEW/output_wikidata_protCL_NEW_${RUN_TAG}/" \
   >> "${LOGFILE}" 2>&1
