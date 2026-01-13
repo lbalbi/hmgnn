d7ede87ca5e92bc7002cd90185cd4eb79e749f94
lbalbi <72658323+lbalbi@users.noreply.github.com>
2026-01-13 11:38:18 +0000

    commit

--- run_parallel.sh
+++ run_parallel.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
-#SBATCH --job-name=huri_perPPI_gcn
-#SBATCH --array=1-1
+#SBATCH --job-name=human_sgat_perProt_NEW
+#SBATCH --array=11-11
 #SBATCH --output=slurm_log.txt
 #SBATCH --ntasks=1
 #SBATCH --time=30:00:00
-#SBATCH --nodelist=liseda-t2
+#SBATCH --nodelist=liseda-03
 #SBATCH --partition=tier3
 
 set -euo pipefail
 
 RUN_TAG="run_${SLURM_ARRAY_TASK_ID}"
-LOG_DIR="output/huri_perPPI_gcn"
-OUTDIR="${LOG_DIR}/output_huri_perPPI_gcn_${RUN_TAG}"
-LOGFILE="${LOG_DIR}/output_huri_perPPI_gcn_${RUN_TAG}.txt"
+LOG_DIR="output/human_sgat_perProt_NEW"
+OUTDIR="${LOG_DIR}/human_sgat_perProt_NEW_${RUN_TAG}"
+LOGFILE="${LOG_DIR}/human_sgat_perProt_NEW_${RUN_TAG}.txt"
 mkdir -p "${OUTDIR}"
 
 python -u main.py \
-  --path "data/huri_data_withLC" \
-  --task "huri" \
+  --path "data/human_data_withLC" \
+  --task "human" \
   --no_contrastive \
-  --model "gcn" \
-  --patience 25 \
-  --output_dir "huri_perPPI_gcn/output_huri_perPPI_gcn_${RUN_TAG}/" \
+  --model "sgat" \
+  --patience 15 \
+  --protein_splits \
+  --finaltrain_only \
+  --batch_size 3024 \
+  --final_lr 0.01 \
+  --final_epochs 40 \
+  --output_dir "human_sgat_perProt_NEW/human_sgat_perProt_NEW_${RUN_TAG}/" \
   >> "${LOGFILE}" 2>&1
