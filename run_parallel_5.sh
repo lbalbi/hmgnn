f8935a3e8c37ad27116fbf85c6c949a1d77315a4
lbalbi <72658323+lbalbi@users.noreply.github.com>
2026-01-14 13:03:10 +0000

    commit

--- run_parallel_5.sh
+++ run_parallel_5.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
-#SBATCH --job-name=human_rahgcn_noNegs_RandomNegs_NEW
-#SBATCH --array=6-6
+#SBATCH --job-name=wikidata_gcn_noNegs_NEW
+#SBATCH --array=1-5
 #SBATCH --output=slurm_log.txt
 #SBATCH --ntasks=1
 #SBATCH --time=30:00:00
-#SBATCH --nodelist=liseda-03
+#SBATCH --nodelist=liseda-05
 #SBATCH --partition=tier3
 
 set -euo pipefail
 
 RUN_TAG="run_${SLURM_ARRAY_TASK_ID}"
-LOG_DIR="output/wikidata_rahgcn_noNegs_RandomNegs_NEW"
-OUTDIR="${LOG_DIR}/output_wikidata_rahgcn_noNegs_RandomNegs_NEW_${RUN_TAG}"
-LOGFILE="${LOG_DIR}/output_wikidata_rahgcn_noNegs_RandomNegs_NEW_${RUN_TAG}.txt"
+LOG_DIR="output/wikidata_gcn_noNegs_NEW"
+OUTDIR="${LOG_DIR}/output_wikidata_gcn_noNegs_NEW_${RUN_TAG}"
+LOGFILE="${LOG_DIR}/output_wikidata_gcn_noNegs_NEW_${RUN_TAG}.txt"
 mkdir -p "${OUTDIR}"
 
 python -u main.py \
   --path "data/wikidata_data" \
   --task "wikidata" \
-  --model "ra_hgcn" \
+  --model "gcn" \
   --batch_size 3048 \
-  --use_rstatement_sampler \
+  --no_contrastive \
   --use_nstatementsampler \
-  --test_only \
-  --output_dir "wikidata_rahgcn_noNegs_RandomNegs_NEW/output_wikidata_rahgcn_noNegs_RandomNegs_NEW_${RUN_TAG}/" \
+  --output_dir "wikidata_gcn_noNegs_NEW/output_wikidata_gcn_noNegs_NEW_${RUN_TAG}/" \
   >> "${LOGFILE}" 2>&1
