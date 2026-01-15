54e7965e129ecef5927b4f75b92209f061549deb
lbalbi <72658323+lbalbi@users.noreply.github.com>
2026-01-15 22:04:09 +0000

    commit

--- run_parallel_2.sh
+++ run_parallel_2.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
-#SBATCH --job-name=human_hgat_protCL_NEW
+#SBATCH --job-name=human_gat_ProtCL_perProt_NEW
 #SBATCH --array=4-4
 #SBATCH --output=slurm_log.txt
 #SBATCH --ntasks=1
 #SBATCH --time=30:00:00
-#SBATCH --nodelist=liseda-03
+#SBATCH --nodelist=liseda-01
 #SBATCH --partition=tier3
 
 set -euo pipefail
 
 RUN_TAG="run_${SLURM_ARRAY_TASK_ID}"
-LOG_DIR="output/human_perProt_hgat_protCL_NEW"
-OUTDIR="${LOG_DIR}/output_human_perProt_hgat_protCL_NEW_${RUN_TAG}"
-LOGFILE="${LOG_DIR}/output_human_perProt_hgat_protCL_NEW_${RUN_TAG}.txt"
+LOG_DIR="output/human_gat_ProtCL_perProt_NEW"
+OUTDIR="${LOG_DIR}/human_gat_ProtCL_perProt_NEW_${RUN_TAG}"
+LOGFILE="${LOG_DIR}/human_gat_ProtCL_perProt_NEW_${RUN_TAG}.txt"
 mkdir -p "${OUTDIR}"
 
 python -u main.py \
   --path "data/human_data_withLC" \
   --task "human" \
-  --model "hgat" \
-  --batch_size 3048 \
+  --model "gat" \
   --protein_splits \
-  --output_dir "human_perProt_hgat_protCL_NEW/output_human_perProt_hgat_protCL_NEW_${RUN_TAG}/" \
+  --batch_size 3024 \
+  --output_dir "human_gat_ProtCL_perProt_NEW/human_gat_ProtCL_perProt_NEW_${RUN_TAG}/" \
   >> "${LOGFILE}" 2>&1
+  #   --patience 15 \
