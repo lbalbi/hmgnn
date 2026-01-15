f41f8465385d990b19ea630de9a5c001b4ed781c
lbalbi <72658323+lbalbi@users.noreply.github.com>
2026-01-15 22:03:15 +0000

    commit

--- run_parallel.sh
+++ run_parallel.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
-#SBATCH --job-name=wikidata_rahgat_noCL_NEW
+#SBATCH --job-name=wikidata_rahgat_protCL_NEW
 #SBATCH --array=1-3
 #SBATCH --output=slurm_log.txt
 #SBATCH --ntasks=1
 #SBATCH --time=30:00:00
-#SBATCH --nodelist=liseda-05
+#SBATCH --nodelist=liseda-01
 #SBATCH --partition=tier3
 
 set -euo pipefail
 
 RUN_TAG="run_${SLURM_ARRAY_TASK_ID}"
-LOG_DIR="output/wikidata_rahgat_noCL_NEW"
-OUTDIR="${LOG_DIR}/output_wikidata_rahgat_noCL_NEW_${RUN_TAG}"
-LOGFILE="${LOG_DIR}/output_wikidata_rahgat_noCL_NEW_${RUN_TAG}.txt"
+LOG_DIR="output/wikidata_rahgat_protCL_NEW"
+OUTDIR="${LOG_DIR}/output_wikidata_rahgat_protCL_NEW_${RUN_TAG}"
+LOGFILE="${LOG_DIR}/output_wikidata_rahgat_protCL_NEW_${RUN_TAG}.txt"
 mkdir -p "${OUTDIR}"
 
 python -u main.py \
   --path "data/wikidata_data" \
   --task "wikidata" \
   --model "ra_hgat" \
-  --no_contrastive \
-  --batch_size 3024 \
-  --output_dir "wikidata_rahgat_noCL_NEW/output_wikidata_rahgat_noCL_NEW_${RUN_TAG}/" \
+  --batch_size 3048 \
+  --output_dir "wikidata_rahgat_protCL_NEW/output_wikidata_rahgat_protCL_NEW_${RUN_TAG}/" \
   >> "${LOGFILE}" 2>&1
