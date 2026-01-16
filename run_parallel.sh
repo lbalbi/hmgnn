3ca3a9d71bddb00cea352a0b2af53f445da4a29f
lbalbi <72658323+lbalbi@users.noreply.github.com>
2026-01-16 12:25:51 +0000

    commit

--- run_parallel.sh
+++ run_parallel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=wikidata_rahgat_protCL_NEW
+#SBATCH --job-name=wikidata_gcn_noNegs_RandomNegs_NEW
 #SBATCH --array=1-3
 #SBATCH --output=slurm_log.txt
 #SBATCH --ntasks=1
@@ -10,15 +10,17 @@
 set -euo pipefail
 
 RUN_TAG="run_${SLURM_ARRAY_TASK_ID}"
-LOG_DIR="output/wikidata_rahgat_protCL_NEW"
-OUTDIR="${LOG_DIR}/output_wikidata_rahgat_protCL_NEW_${RUN_TAG}"
-LOGFILE="${LOG_DIR}/output_wikidata_rahgat_protCL_NEW_${RUN_TAG}.txt"
+LOG_DIR="output/wikidata_gcn_noNegs_RandomNegs_NEW"
+OUTDIR="${LOG_DIR}/output_wikidata_gcn_noNegs_RandomNegs_NEW_${RUN_TAG}"
+LOGFILE="${LOG_DIR}/output_wikidata_gcn_noNegs_RandomNegs_NEW_${RUN_TAG}.txt"
 mkdir -p "${OUTDIR}"
 
 python -u main.py \
   --path "data/wikidata_data" \
   --task "wikidata" \
-  --model "ra_hgat" \
+  --model "gcn" \
+  --use_nstatementsampler \
+  --use_rstatement_sampler \
   --batch_size 3048 \
-  --output_dir "wikidata_rahgat_protCL_NEW/output_wikidata_rahgat_protCL_NEW_${RUN_TAG}/" \
+  --output_dir "wikidata_gcn_noNegs_RandomNegs_NEW/output_wikidata_gcn_noNegs_RandomNegs_NEW_${RUN_TAG}/" \
   >> "${LOGFILE}" 2>&1
