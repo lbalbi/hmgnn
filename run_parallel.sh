ead0b0bea608d1ad60d70641d58f2bc2d211086a
lbalbi <72658323+lbalbi@users.noreply.github.com>
2026-01-16 22:04:03 +0000

    commit

--- run_parallel.sh
+++ run_parallel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=wikidata_gcn_noNegs_RandomNegs_NEW
-#SBATCH --array=1-3
+#SBATCH --array=3-3
 #SBATCH --output=slurm_log.txt
 #SBATCH --ntasks=1
 #SBATCH --time=30:00:00
