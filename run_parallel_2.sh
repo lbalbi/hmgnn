609493b70a401f3f6d09528e59f356d6c30cf307
lbalbi <72658323+lbalbi@users.noreply.github.com>
2026-01-16 12:31:05 +0000

    commit

--- run_parallel_2.sh
+++ run_parallel_2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=human_gat_ProtCL_perProt_NEW
-#SBATCH --array=4-4
+#SBATCH --job-name=human_hgcn_noNegs_noCL_perProt_NEW
+#SBATCH --array=1-3
 #SBATCH --output=slurm_log.txt
 #SBATCH --ntasks=1
 #SBATCH --time=30:00:00
@@ -10,17 +10,19 @@
 set -euo pipefail
 
 RUN_TAG="run_${SLURM_ARRAY_TASK_ID}"
-LOG_DIR="output/human_gat_ProtCL_perProt_NEW"
-OUTDIR="${LOG_DIR}/human_gat_ProtCL_perProt_NEW_${RUN_TAG}"
-LOGFILE="${LOG_DIR}/human_gat_ProtCL_perProt_NEW_${RUN_TAG}.txt"
+LOG_DIR="output/human_hgcn_noNegs_noCL_perProt_NEW"
+OUTDIR="${LOG_DIR}/human_hgcn_noNegs_noCL_perProt_NEW_${RUN_TAG}"
+LOGFILE="${LOG_DIR}/human_hgcn_noNegs_noCL_perProt_NEW_${RUN_TAG}.txt"
 mkdir -p "${OUTDIR}"
 
 python -u main.py \
   --path "data/human_data_withLC" \
   --task "human" \
-  --model "gat" \
+  --model "hgcn" \
   --protein_splits \
+  --use_nstatement_sampler \
+  --no_contrastive \
   --batch_size 3024 \
-  --output_dir "human_gat_ProtCL_perProt_NEW/human_gat_ProtCL_perProt_NEW_${RUN_TAG}/" \
+  --output_dir "human_hgcn_noNegs_noCL_perProt_NEW/human_hgcn_noNegs_noCL_perProt_NEW_${RUN_TAG}/" \
   >> "${LOGFILE}" 2>&1
   #   --patience 15 \
