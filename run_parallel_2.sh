0fa4c52ee99d7422883b637d111bb33fe2ff6e7e
lbalbi <72658323+lbalbi@users.noreply.github.com>
2026-02-09 20:11:20 +0000

    m

--- run_parallel_2.sh
+++ run_parallel_2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=wikidata_test_rargcn_randomNegs
+#SBATCH --job-name=wikidata_test_rargcn_protCL_NEW
 #SBATCH --array=1-2
-#SBATCH --output=slurm_log.txt
+#SBATCH --output=slurm_log2.txt
 #SBATCH --ntasks=1
 #SBATCH --time=30:00:00
 #SBATCH --nodelist=liseda-05
@@ -15,9 +15,9 @@ ulimit -n 65535
 set -euo pipefail
 
 RUN_TAG="run_${SLURM_ARRAY_TASK_ID}"
-LOG_DIR="output/wikidata_test_rargcn_randomNegs"
-OUTDIR="${LOG_DIR}/output_wikidata_test_rargcn_randomNegs_${RUN_TAG}"
-LOGFILE="${LOG_DIR}/output_wikidata_test_rargcn_randomNegs_${RUN_TAG}.txt"
+LOG_DIR="output/wikidata_test_rargcn_protCL_NEW"
+OUTDIR="${LOG_DIR}/output_wikidata_test_rargcn_protCL_NEW_${RUN_TAG}"
+LOGFILE="${LOG_DIR}/output_wikidata_test_rargcn_protCL_NEW_${RUN_TAG}.txt"
 mkdir -p "${OUTDIR}"
 
 python -u /home/lbalbi/datasets/hmgnn/main.py \
@@ -25,9 +25,8 @@ python -u /home/lbalbi/datasets/hmgnn/main.py \
   --task "wikidata" \
   --model "ra_rgcn" \
   --batch_size 18640 \
-  --use_rstatement_sampler \
   --parallel_grid \
   --parallel_grid_workers 2 \
   --parallel_grid_loader_workers 0 \
-  --output_dir "wikidata_test_rargcn_randomNegs/output_wikidata_test_rargcn_randomNegs_${RUN_TAG}/" \
+  --output_dir "wikidata_test_rargcn_protCL_NEW/output_wikidata_test_rargcn_protCL_NEW_${RUN_TAG}/" \
   >> "${LOGFILE}" 2>&1
